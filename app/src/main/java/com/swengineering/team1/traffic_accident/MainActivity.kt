6c28c61d696ac2fcb1138b88ff3170cfb1957c15
reasure3 <reasure3@naver.com>
2025-05-21 19:28:54 +0900

    Initial Commit

---
+++ app/src/main/java/com/swengineering/team1/traffic_accident/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.swengineering.team1.traffic_accident
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.swengineering.team1.traffic_accident.ui.theme.TrafficAccidentAlertMonitoringTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TrafficAccidentAlertMonitoringTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TrafficAccidentAlertMonitoringTheme {
+        Greeting("Android")
+    }
+}
