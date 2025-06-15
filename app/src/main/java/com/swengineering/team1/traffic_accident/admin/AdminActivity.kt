70f786fe78358eb6ea59578fc71a2e0ba47ec3f1
reasure3 <reasure3@naver.com>
2025-06-15 19:23:39 +0900

    ConfigSettingsFragment - ConfigHistoryFragment 변경 버튼 추가

--- app/src/main/java/com/swengineering/team1/traffic_accident/admin/AdminActivity.kt
+++ app/src/main/java/com/swengineering/team1/traffic_accident/admin/AdminActivity.kt
@@ -1,18 +1,43 @@
 package com.swengineering.team1.traffic_accident.admin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.swengineering.team1.traffic_accident.R
 
 class AdminActivity : AppCompatActivity() {
+    private lateinit var btnSettings: Button
+    private lateinit var btnHistory: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
 
+        btnSettings = findViewById(R.id.btn_settings)
+        btnHistory = findViewById(R.id.btn_history)
+
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, ConfigSettingsFragment())
-                .commit()
+            showSettingsFragment()
         }
+
+        btnSettings.setOnClickListener {
+            showSettingsFragment()
+        }
+
+        btnHistory.setOnClickListener {
+            showListFragment()
+        }
+    }
+
+    private fun showSettingsFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, ConfigSettingsFragment())
+            .commit()
+    }
+
+    private fun showListFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, ConfigHistoryFragment())
+            .commit()
     }
 } 
